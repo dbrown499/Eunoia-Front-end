387035bd929e3a8f45a4d6c78e3679c55e2b1cd5
Diamond <dbrown@pursuit.org>
2025-02-12 02:24:53 -0500

    edited home page styling

--- src/Components/ContactInfo.jsx
+++ src/Components/ContactInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Styling/ContactInfo.scss'
-// import profilePic from '../../assets/1EB45B7F-F79A-4C3A-9F70-2D2B6719A4AA.JPEG'
+
+
 const ContactInfo = () => {
   return (
 
@@ -53,24 +54,11 @@ const ContactInfo = () => {
       <div className='copyright-tag pay-images'>
         <p className='copyright'>© 2024 ŒUNÖIA. All rights reserved.</p>
         <div className='cards'>
-          <img className="each-card" src="Screenshot 2025-02-11 at 9.47.27 PM.png" alt="cards" />
+          <img className="each-card" src="assets/Screenshot 2025-02-12 at 2.11.37 AM.png" alt="cards" />
         </div>
       </div>
     </section>
 
-    // <section className='contact_developer'>
-    //   {/* <img src={profilePic} alt="description"/> */}
-    //   <p className='contact'>CONTACT US</p>
-    //   <section className='contact-images'>
-    //     <a href="https://www.instagram.com/eunoia.outlet/?igsh=dWgzc20yanJxMWV2" target="_blank">
-    //     <img src="assets/instagramicon.png" className="instagram-image " alt="" />
-    //     </a>
-    //     <a href="https://www.tiktok.com/@eunoia.outlet?_t=8r6Yo1Sfsxq&_r=1" target="_blank">
-    //     <img src="assets/tiktokicon.png" className="tiktok-image" alt="" />
-    //   </a>
-    //     </section>
-    // <p className='copyright'>© 2024 ŒUNÖIA. All rights reserved.</p>
-    // </section>
   )
 }
 
